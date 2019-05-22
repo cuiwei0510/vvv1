6ce045c0a715d20345c030f15407e764ec70350d
cuiwei0510 <cuiwei0510@126.com>
2019-05-22 13:54:50 +0800

    Initial commit

---
+++ src/vvv/MyClass.java
@@ -0,0 +1,11 @@
+package vvv;
+
+/**
+ * @author cuiwei
+ * @create 2019-05-22 13:50
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("vvv1");
+    }
+}
